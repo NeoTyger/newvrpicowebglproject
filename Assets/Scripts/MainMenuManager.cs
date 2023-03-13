b46976860b3d6ef54843f8ba41a7fafe4dd69993
NeoTyger <neojl333@gmail.com>
2023-03-13 13:17:36 +0100

    Main Menu botones funcionales.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private Button _startGameBtn;
+    [SerializeField] private Button btnPlay;
+    [SerializeField] private Button btnExit;
+    [SerializeField] private Button btnOptions;
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Level 1");
     }
-    
-    
+
+    public void ToOptionsMenu()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
+
+    public void Exitgame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        _startGameBtn.onClick.AddListener(StartGame);
-        Debug.Log("Main menu manager initialized");
+        btnPlay.onClick.AddListener(StartGame);
+        btnExit.onClick.AddListener(Exitgame);
+        btnOptions.onClick.AddListener(ToOptionsMenu);
     }
 }
